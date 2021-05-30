ad10ffde8eb644cd8c516a56e55db73d0d30d476
Christiaan Hougaard <cwhougaard@gmail.com>
2021-05-30 13:23:42 -0400

    added ToDo class

--- script.js
+++ script.js
@@ -5,12 +5,19 @@ const toDoItems = {
   "todo" : 'mowe the lawn'
 }
 
+class ToDo {
+  constructor (item) {
+    this.done = false
+    this.todo = item
+  }
+}
 
+
+// Grab todo from Input and push to Object
 $('#form').on('keypress',function(e) {
   if(e.which == 13) {
     let todo = $('.form-control').val()
     $('.form-control').val("")
-    console.log(todo);
     e.preventDefault()
   }
 });
